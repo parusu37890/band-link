9bc95fd888abf940aecb066bc9e3d9797f85ceea
parusu37890 <parusu37890@gmail.com>
2026-09-19 18:48:13 +0900

    Revert "feat(auth): add X (Twitter) login, mirroring the LINE OAuth flow"
    This reverts commit 2cb4802f815e419633686c2cdb115a98e22f3190.

--- src/main/java/com/example/bandlink/repository/XAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bandlink.repository;
-
-import com.example.bandlink.entity.XAccount;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface XAccountRepository extends JpaRepository<XAccount, Long> {
-    Optional<XAccount> findByXUserId(String xUserId);
-}
